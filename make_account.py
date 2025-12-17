bfe3cd6fb750d09f0ca28493c38e1cd822fd9d66
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-12-17 13:50:47 +0100

    make_account.py

--- make_account.py
+++ make_account.py
@@ -1,7 +1,4 @@
-def is_odd(k):
-    """
-    Return True if k is odd, otherwise return False.
-    """
+def make_account(initial_balance):
     ### Replace with your own code (begin) ###
     pass
     ### Replace with your own code (end)   ###
